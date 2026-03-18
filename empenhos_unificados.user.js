e1f00c609ec2efc8fe98434d875326114291cabc
erikhigino-esh <erikhigino@gmail.com>
2026-03-18 09:54:07 -0300

    Update empenhos_unificados.user.js

--- empenhos_unificados.user.js
+++ empenhos_unificados.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
-// @name         Empenhos Unificados AUTO + UI (detecta ano 2025/2026)
+// @name         Empenhos Unificados
 // @namespace    https://ofcweb.inss.gov.br/
-// @version      2.2
+// @version      2.3
 // @description  Preenche automaticamente o Nº do Empenho no PCO conforme o ITEM da AP. Detecta automaticamente o ano selecionado (2025 ou 2026) e aplica os compromissos correspondentes. Versão com interface visual.
 // @match        https://ofcweb.inss.gov.br/*
 // @match        http://ofcweb.inss.gov.br/*
