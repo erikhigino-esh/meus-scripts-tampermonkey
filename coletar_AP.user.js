896bad2a1b025975b4d5b04f05be869c36a12726
erikhigino-esh <erikhigino@gmail.com>
2026-03-20 11:44:04 -0300

    Update coletar_AP.user.js

--- coletar_AP.user.js
+++ coletar_AP.user.js
@@ -1,7 +1,8 @@
 // ==UserScript==
 // @name         Coletar APs
 // @namespace    http://tampermonkey.net/
-// @version      1.2
+// @autor        Erik Higino
+// @version      1.3
 // @description  Captura APs SOMENTE da coluna "Nº AP" e envia ao servidor unificado. Forçado para 2025/2026.
 // @match        https://ofcweb.inss.gov.br/View/Registrar_Analise_Ap_OFC.php*
 // @grant        GM_xmlhttpRequest
