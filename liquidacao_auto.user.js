08c88079f911b6f2cce7dd8521293e5ae7235e57
erikhigino-esh <erikhigino@gmail.com>
2026-09-10 17:46:04 -0300

    Update liquidacao_auto.user.js

--- liquidacao_auto.user.js
+++ liquidacao_auto.user.js
@@ -2,7 +2,7 @@
 // @name         Liquidação Automática
 // @namespace    http://tampermonkey.net/
 // @author       Erik Higino
-// @version      6.0
+// @version      6.1
 // @description  Auto-liquidação DH. Detecta automaticamente o ano (2025/2026), seleção de ITEMs obrigatória, delays (5s/5s/5s), toggle moderno e resiliente. Preenche data de emissão contábil automaticamente. Ignora APs com erro de validação SIAFI e segue para a próxima.
 // @match        https://ofcweb.inss.gov.br/View/Consultar_Liquidar.php*
 // @match        https://ofcweb.inss.gov.br/View/Define_Formulario_Liquidacao_DH.php*
